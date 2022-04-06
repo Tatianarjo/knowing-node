439ae52d8aa133e6e77dad93d87851e6836da53f
Tatiana Johnson <Tatianarjo@gmail.com>
2022-04-05 22:36:23 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Modules
+// CommonJS, every file is a module (by default)
+const peaches = 'peaches'
+// const peter = 'peter'
+
+const sayHi = (name) => {
+    console.log(`Hello there ${name}`)
+}
+
+sayHi('susan')
+sayHi(peaches)
+sayHi('john')
